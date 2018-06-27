70f6bc818ec7fc90ab943bad9182ca2c0121ce46
Tyler <tganyan@gmail.com>
2018-06-27 15:15:05 -0700

    still not quite there, but cleaner. runSimon() is current implementation function

--- js/game.js
+++ js/game.js
@@ -21,20 +21,6 @@ for (var i = 0; i < userControls.length; i++) {
 
 // Helpers
 
-
-function addClass(element,value){
-	if(element.className != value){
-		element.classList.add(value)
-	}
-}
-
-function removal(element, value){
-	for(var i=0; i < gameSquares.length; i++) {
-		element.classlist.remove(value) 
-	}	
-}
-
-
 function getRandom () {
 	var randomIndex = Math.floor(Math.random() * simonColors.length)
 	return randomIndex
@@ -46,69 +32,127 @@ function getRandomID () {
 	return id
 }
 
+function getElement () {
+	var random = getRandomID()
+	var el = document.getElementById(random)
+	return el
+}
+
+
+// function toggleHighlight(element){
+	
+// 	if(element.classList.contains('highlight')){
+// 		element.classList.remove('highlight')
+// 	} else {
+// 		element.classList.add('highlight')
+// 	}
+// }
+
 function pushSimon (index) {
 	simonPicks.push(simonColors[index])
+}
+
+
+function pulse () {
+	var element = getElement();
+	element.classList.add('highlight')
+	setTimeout(function () {
+		element.classList.remove('highlight')
+	}, 300)
+
+	// for (var i = 0; i < counter; i++) {
+	// 	// element = getElement()
+	// 	// toggleHighlight(element)
+	// 	intervalis()
+	// 	clearInterval(intervalis)
+	// }	
+}
+
+function renderPulse () {
+	var i = 0;
+	var pulser = setInterval(function () {
+		pulse()
+		i++
+		if (i < 5) {
+			clearInterval(pulser)
+		}
+	}, 600)
+}
+
+
+function runSimon () {
+	for (var i = 0; i < 5; i++) {
+		renderPulse()
+	}
 }
 
 // var random = getRandom();  pushSimon(random)
 
 
 
-function simon (index, id) {
-		setInterval(function() {
-			// removal(id[index], 'highlight')
-			// addClass(id[index], 'highlight')
-			console.log(id[index])
-		}, 800)
-		// console.log(gameSquares[index])
-}
-
-
-var randomIndex = getRandom()
-var randomID = simonColors[randomIndex]
 
 
 
-simon(randomIndex, randomID)
-// function runTheGame () {
 
-// }
+
+
 
 
 // function simon (index, id) {
-// 	var element = document.getElementById(id).classList.add('highlight')
-// 	setInterval(function() {
-// 		document.getElementById(id).classList.remove('highlight')
-// 		simonPicks.push(simonColors[index])
-// 		console.log(`remove ${id}`)
-// 	}, 800)
-
-
-// 	var j = -1;
-
-// 	setInterval(function () {
-// 			removeall();
-// 			j++;
-// 			toggleClass(divs[j],"red");	
-// 		},2000);
+// 		setInterval(function() {
+// 			// removal(id[index], 'highlight')
+// 			// addClass(id[index], 'highlight')
+// 			console.log(id[index])
+// 		}, 800)
+// 		// console.log(gameSquares[index])
 // }
 
 
+// var randomIndex = getRandom()
+// var randomID = simonColors[randomIndex]
 
-// function runTheGame() {
-// 	var picksCounter = 4;
-// 	var firstDigit = 3;
-// 	var timeout = firstDigit + 0 + 0 + 0;
-// 	do {		
-// 		for (var i = 0; i < picksCounter; i++) {
-// 			var randomIndex = Math.floor(Math.random() * simonColors.length)
-// 			var sqrID = simonColors[randomIndex]
-// 			simon(randomIndex, sqrID)
-// 		}
-// 		simonPicks = []
-// 		picksCounter++
-// 	}while (picksCounter < 5)
-// }
+
+
+// simon(randomIndex, randomID)
+// // function runTheGame () {
+
+// // }
+
+
+// // function simon (index, id) {
+// // 	var element = document.getElementById(id).classList.add('highlight')
+// // 	setInterval(function() {
+// // 		document.getElementById(id).classList.remove('highlight')
+// // 		simonPicks.push(simonColors[index])
+// // 		console.log(`remove ${id}`)
+// // 	}, 800)
+
+
+// // 	var j = -1;
+
+// // 	setInterval(function () {
+// // 			removeall();
+// // 			j++;
+// // 			toggleClass(divs[j],"red");	
+// // 		},2000);
+// // }
+
+
+
+// // function runTheGame() {
+// // 	var picksCounter = 4;
+// // 	var firstDigit = 3;
+// // 	var timeout = firstDigit + 0 + 0 + 0;
+// // 	do {		
+// // 		for (var i = 0; i < picksCounter; i++) {
+// // 			var randomIndex = Math.floor(Math.random() * simonColors.length)
+// // 			var sqrID = simonColors[randomIndex]
+// // 			simon(randomIndex, sqrID)
+// // 		}
+// // 		simonPicks = []
+// // 		picksCounter++
+// // 	}while (picksCounter < 5)
+// // }
 
 
 
