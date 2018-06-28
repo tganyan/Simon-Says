e2d05eb7a1b7d782bf43b1cb593564d799526a0c
Tyler <tganyan@gmail.com>
2018-06-27 17:42:09 -0700

    making updates to markup and css

--- js/app.js
+++ js/app.js
@@ -2,19 +2,33 @@ function loadLanding () {
 		document.getElementById('user-submit').addEventListener('click',  function() {
 			addUser(getUserNames()[0], getUserNames()[1]);
 			document.getElementById('sign-in').classList.add('faded');
+			document.getElementById('sign-in').classList.add('shrink');
 			document.getElementById('game-app').classList.remove('faded');
 			renderTopTen()
-			renderCurrentUser('name', 'h3', 'rank-game')
+			renderCurrentUser('name', 'h3', 'rank-game', true)
 		})
 }
 
 
 // Render current username
 
-function renderCurrentUser (targetID, headerLevel, rankID) {
+function renderCurrentUser (targetID, headerLevel, rankID, isLink) {
 	var currentUser = getUserNames()
 	var firstLast = getUserNames()[0].split(' ')
-	document.getElementById(targetID).innerHTML += `<${headerLevel}>${firstLast[0]} "${currentUser[1]}" ${firstLast[1]}</${headerLevel}>`
+	var linkStart;
+	var linkEnd;
+	var targetElement = document.getElementById(targetID);
+
+	targetElement.innerHTML += `<div class="name-font-container"><${headerLevel}>${firstLast[0]} "${currentUser[1]}" ${firstLast[1]}</${headerLevel}></div>`
+
+	if (isLink) {
+		targetElement.classList.add('hov')
+		targetElement.addEventListener('click', function() {
+			document.getElementById('game-app').classList.add('faded');
+			document.getElementById('game-app').classList.add('shrink');
+			document.getElementById('profile-page').classList.remove('faded');
+		})
+	}
 	renderCurrentRank(rankID)
 }
 
